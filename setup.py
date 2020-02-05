5d630817e3334ebd67c41df954ec6fd401016565
sourmpis <chris.sourmp@gmail.com>
2020-02-05 18:23:25 +0100

    version 0.0.5 change in the setup

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ packages_pre = ["%s.%s" % (prefix, s) for s in packages]
 
 setup(
   name='neurodynex3',
-  version = '0.0.4',
+  version = '0.0.5',
   packages=find_packages(),
   package_data={
     'neurodynex3': ['data/*'],
