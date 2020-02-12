61d381e7ff6f45cec2619a5031e0722412694a16
martin barry <barry@tsf-484-wpa-1-121.epfl.ch>
2020-02-12 12:29:34 +0100

    weird flex in gitignore

--- doc/_build/html/_static/documentation_options.js
+++ doc/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.5.dev49+gd411808.d20200212',
+    VERSION: '0.0.5.dev51+g2e5d855.d20200212',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
